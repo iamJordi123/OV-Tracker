9ec2f0a8b989e1192effc1e542a8e1a15abf548d
iamJordi123 <j.lukkes@proton.me>
2026-01-29 20:15:39 +0100

    .

--- api/get-buses.js
+++ api/get-buses.js
@@ -1,30 +1,27 @@
 export default async function handler(req, res) {
+    // Stel headers in zodat de browser weet dat er JSON aankomt
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
 
     try {
-        // We gebruiken de betrouwbare v0 JSON bron
+        // We gebruiken een kortere timeout en de v0 endpoint
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), 8000); // Stop na 8 sec
+
         const response = await fetch('https://v0.ovapi.nl/vehicle/', {
-            headers: {
-                'User-Agent': 'VercelBusTrackerProject/1.0'
-            }
+            signal: controller.signal,
+            headers: { 'User-Agent': 'MyPersonalTracker/1.0' }
         });
 
-        if (!response.ok) throw new Error('OVapi is tijdelijk overbelast');
+        if (!response.ok) throw new Error('OVapi is bezet');
 
         const data = await response.json();
         
-        // We zetten het OVapi object om naar een simpel lijstje voor je kaart
-        const vehicles = Object.keys(data).map(key => ({
-            id: key,
-            lat: data[key].Latitude,
-            lon: data[key].Longitude,
-            line: data[key].LinePublicNumber
-        }));
-
-        return res.status(200).json(vehicles);
+        // We sturen de data direct door zonder zware bewerkingen
+        return res.status(200).send(JSON.stringify(data));
     } catch (error) {
-        // Backup bus zodat je ziet dat de verbinding werkt
-        return res.status(200).json([{ id: "TEST", lat: 52.1326, lon: 5.2913 }]);
+        // Als OVapi te traag is, stuur dan een lege lijst in plaats van een 500 error
+        console.error("OVapi timeout of error");
+        return res.status(200).json({});
     }
 }
--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const map = L.map('map').setView([52.1326, 5.2913], 8);
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').addTo(map);
-
-let busMarkers = {};
+// ... (map setup)
 
 async function updateBuses() {
     try {
         const response = await fetch('/api/get-buses');
-        const vehicles = await response.json();
+        const data = await response.json();
         
-        vehicles.forEach((v) => {
-            if (v.lat && v.lon) {
-                if (busMarkers[v.id]) {
-                    busMarkers[v.id].setLatLng([v.lat, v.lon]);
+        // Verander het object van OVapi in een lijst die we kunnen tekenen
+        const vehicleKeys = Object.keys(data);
+        console.log("Echte OVapi bussen gevonden:", vehicleKeys.length);
+
+        vehicleKeys.forEach((key) => {
+            const v = data[key];
+            if (v.Latitude && v.Longitude) {
+                if (busMarkers[key]) {
+                    busMarkers[key].setLatLng([v.Latitude, v.Longitude]);
                 } else {
-                    busMarkers[v.id] = L.circleMarker([v.lat, v.lon], {
+                    busMarkers[key] = L.circleMarker([v.Latitude, v.Longitude], {
                         radius: 4,
                         fillColor: "#ffc917",
                         color: "#000",
                         weight: 1,
-                        fillOpacity: 0.8
+                        fillOpacity: 0.9
                     }).addTo(map);
                 }
             }
         });
-    } catch (e) { console.log("Wachten op data..."); }
+    } catch (e) {
+        console.log("Wachten op OVapi respons...");
+    }
 }
-
-setInterval(updateBuses, 10000);
-updateBuses();
