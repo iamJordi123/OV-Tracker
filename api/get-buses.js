0b89076c317f367bf66a3c00109ba543da238c18
iamJordi123 <j.lukkes@proton.me>
2026-01-29 19:58:45 +0100

    .

--- api/get-buses.js
+++ api/get-buses.js
@@ -1,23 +1,33 @@
 export default async function handler(req, res) {
-    // We gebruiken nu de Data.ndovloket.nl bron, deze is vaak stabieler
-    const url = 'https://data.ndovloket.nl/all/bussen.json'; 
-    
-    try {
-        const response = await fetch(url);
-        
-        if (!response.ok) {
-            // Als deze ook plat ligt, proberen we een backup URL
-            const backupRes = await fetch('https://v0.ovapi.nl/vehicle/');
-            const backupData = await backupRes.json();
-            return res.status(200).json(backupData);
-        }
+    // Lijst met bronnen die we gaan proberen
+    const sources = [
+        'https://data.ndovloket.nl/all/bussen.json',
+        'https://api.ovapi.nl/vehicle/',
+        'https://v0.ovapi.nl/vehicle/'
+    ];
 
-        const data = await response.json();
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Content-Type', 'application/json');
-        return res.status(200).json(data);
-    } catch (error) {
-        // Als alles faalt, sturen we een lege lijst zodat je kaart niet crasht
-        return res.status(200).json({});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+
+    for (let url of sources) {
+        try {
+            console.log("Proberen:", url);
+            const response = await fetch(url, { signal: AbortSignal.timeout(5000) });
+            if (!response.ok) continue;
+
+            const data = await response.json();
+            
+            // Check of er echt data in zit
+            const keys = Object.keys(data);
+            if (keys.length > 0) {
+                console.log("Data gevonden op:", url);
+                return res.status(200).json(data);
+            }
+        } catch (e) {
+            console.log("Faal op:", url);
+            continue;
+        }
     }
+
+    return res.status(500).json({ error: "Alle bronnen zijn momenteel leeg of onbereikbaar" });
 }
