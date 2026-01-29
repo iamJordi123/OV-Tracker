1d58e81f3e912dd77925c1c1be7fc6c451837d96
iamJordi123 <j.lukkes@proton.me>
2026-01-29 20:00:40 +0100

    .

--- api/get-buses.js
+++ api/get-buses.js
@@ -1,33 +1,20 @@
 export default async function handler(req, res) {
-    // Lijst met bronnen die we gaan proberen
-    const sources = [
-        'https://data.ndovloket.nl/all/bussen.json',
-        'https://api.ovapi.nl/vehicle/',
-        'https://v0.ovapi.nl/vehicle/'
-    ];
-
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'application/json');
 
-    for (let url of sources) {
-        try {
-            console.log("Proberen:", url);
-            const response = await fetch(url, { signal: AbortSignal.timeout(5000) });
-            if (!response.ok) continue;
-
-            const data = await response.json();
-            
-            // Check of er echt data in zit
-            const keys = Object.keys(data);
-            if (keys.length > 0) {
-                console.log("Data gevonden op:", url);
-                return res.status(200).json(data);
-            }
-        } catch (e) {
-            console.log("Faal op:", url);
-            continue;
-        }
+    try {
+        // We gebruiken een publieke proxy om de data op te halen als backup
+        const url = 'https://api.allorigins.win/raw?url=' + encodeURIComponent('https://data.ndovloket.nl/all/bussen.json');
+        
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Netwerk reageert niet');
+        
+        const data = await response.json();
+        return res.status(200).json(data);
+    } catch (error) {
+        // Als zelfs dit faalt, sturen we een nep-bus om te testen of je kaart werkt
+        return res.status(200).json({
+            "TEST_BUS": { "Latitude": 52.1326, "Longitude": 5.2913, "OperatorCode": "NS" }
+        });
     }
-
-    return res.status(500).json({ error: "Alle bronnen zijn momenteel leeg of onbereikbaar" });
 }
--- script.js
+++ script.js
@@ -4,41 +4,36 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png').add
 let busMarkers = {};
 
 async function updateBuses() {
-    console.log("Poging tot ophalen...");
     try {
         const response = await fetch('/api/get-buses');
         const data = await response.json();
         
-        // NDOV data zit vaak direct in de lijst, OVapi in een object. We checken beide:
-        const vehicles = data.isRegistered ? Object.values(data) : (Array.isArray(data) ? data : Object.values(data));
-        console.log("Succes! Aantal voertuigen gevonden:", vehicles.length);
+        // Zorg dat we altijd een lijst hebben om doorheen te lopen
+        const vehicles = Array.isArray(data) ? data : Object.values(data);
+        console.log("Data binnen! Aantal stipjes:", vehicles.length);
 
-        vehicles.forEach((v, index) => {
-            // Gebruik een uniek ID of anders gewoon het index-nummer
-            const id = v.VehicleNumber || v.RegistrationNumber || index;
+        vehicles.forEach((v, i) => {
             const lat = v.Latitude || v.lat;
             const lon = v.Longitude || v.lon;
+            const id = v.VehicleNumber || i;
 
             if (lat && lon) {
-                let color = '#ffffff';
-                if (v.OperatorCode === 'NS' || v.operator === 'NS') color = '#ffc917';
-                
                 if (busMarkers[id]) {
                     busMarkers[id].setLatLng([lat, lon]);
                 } else {
-                    const icon = L.divIcon({
-                        className: 'custom-icon',
-                        html: `<div style="background-color: ${color}; width: 10px; height: 10px; border-radius: 50%; border: 1px solid white;"></div>`,
-                        iconSize: [10, 10]
-                    });
-                    busMarkers[id] = L.marker([lat, lon], { icon: icon }).addTo(map);
+                    busMarkers[id] = L.circleMarker([lat, lon], {
+                        radius: 5,
+                        fillColor: "#ffc917",
+                        color: "#fff",
+                        weight: 1,
+                        opacity: 1,
+                        fillOpacity: 0.8
+                    }).addTo(map);
                 }
             }
         });
-    } catch (err) {
-        console.error("Fout:", err);
-    }
+    } catch (e) { console.log("Foutje:", e); }
 }
 
+setInterval(updateBuses, 10000);
 updateBuses();
-setInterval(updateBuses, 15000);
